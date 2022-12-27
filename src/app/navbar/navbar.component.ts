2a05b6c8566a239812451ac5864337f56e379b25
Dennis Lysenko <dennislysenko@hotmail.com>
2022-12-27 16:53:54 -0500

    Implemented resizing searchbar to Angular

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,8 +1,30 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
 
-@Component({
-	selector: 'app-navbar',
-	templateUrl: './navbar.component.html',
-	styleUrls: ['./navbar.component.less'],
-})
-export class NavbarComponent {}
+@Component({ selector: 'app-navbar', templateUrl: './navbar.component.html', styleUrls: ['./navbar.component.less'] })
+export class NavbarComponent implements AfterViewInit {
+	constructor(private render: Renderer2) {}
+
+	ngAfterViewInit(): void {
+		this.resizeNavbar();
+	}
+
+	resizeNavbar() {
+		var MARGIN = 20;
+		var windowWidth = window.innerWidth;
+		var searchContainerWidth = this.searchContainer?.nativeElement.offsetWidth;
+		var navButtonsWidth = this.navButtons?.nativeElement.offsetWidth;
+		var diff = (windowWidth - searchContainerWidth) / 2 - navButtonsWidth - MARGIN;
+		if (diff < 0) {
+			this.render.setStyle(this.searchBar?.nativeElement, 'width', `${searchContainerWidth + diff}px`);
+		} else {
+			this.render.setStyle(this.searchBar?.nativeElement, 'width', `${searchContainerWidth}px`);
+		}
+	}
+
+	@ViewChild('searchContainer')
+	searchContainer: ElementRef | undefined;
+	@ViewChild('searchBar')
+	searchBar: ElementRef | undefined;
+	@ViewChild('navButtons')
+	navButtons: ElementRef | undefined;
+}
