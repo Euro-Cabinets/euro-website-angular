51f815a137355537e28287db8fd82e300ef199fa
Dennis Lysenko <dennislysenko@hotmail.com>
2023-01-06 19:46:45 -0500

    Restructured Project File Hiarchey

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './footer/footer.component';
-import { HomeComponent } from './home/home.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { ShopComponent } from './shop/shop.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ShopComponent } from './pages/shop/shop.component';
 
 @NgModule({
 	declarations: [AppComponent, HomeComponent, NavbarComponent, FooterComponent, ShopComponent],
