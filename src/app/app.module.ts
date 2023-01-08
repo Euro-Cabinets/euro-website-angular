239b8ff85b29d8a0e0e46638c654c7a392f3e981
Dennis Lysenko <dennislysenko@hotmail.com>
2023-01-08 05:29:20 -0500

    Added navigation comp.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,10 @@ import { HomeComponent } from './pages/home/home.component';
 import { ShopComponent } from './pages/shop/shop.component';
 import { SearchBarComponent } from './components/navbar/search-bar/search-bar.component';
 import { NavButtonsComponent } from './components/navbar/nav-buttons/nav-buttons.component';
+import { NavigationComponent } from './components/navbar/navigation/navigation.component';
 
 @NgModule({
-	declarations: [AppComponent, HomeComponent, NavbarComponent, FooterComponent, ShopComponent, SearchBarComponent, NavButtonsComponent],
+	declarations: [AppComponent, HomeComponent, NavbarComponent, FooterComponent, ShopComponent, SearchBarComponent, NavButtonsComponent, NavigationComponent],
 	imports: [BrowserModule, AppRoutingModule],
 	providers: [],
 	bootstrap: [AppComponent],
