4270cf8601ec26cd7d2739220c84d9d077446e80
Dennis Lysenko <dennislysenko@hotmail.com>
2023-01-10 00:02:31 -0500

    Organized CSS files, and added image-view buttons

--- src/app/components/product-list/product-item/product-item.ts
+++ src/app/components/product-list/product-item/product-item.ts
@@ -3,6 +3,12 @@ import { Component } from '@angular/core';
 @Component({
 	selector: 'app-product-item',
 	templateUrl: './product-item.html',
-	styleUrls: ['./product-item.less'],
+	styleUrls: [
+		'./product-item.less',
+		'./_cart-functions.less',
+		'./_options-container.less',
+		'./_product-info.less',
+		'./_icon-set.less',
+	],
 })
 export class ProductItemComponent {}
